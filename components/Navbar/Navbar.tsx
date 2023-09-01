6e7cb07960747728216199c2aa4bd4489e5006e8
Al366io <alessio.nannipieri@viaplaygroup.com>
2023-09-01 07:43:28 +0200

    feat: add logos

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,7 +2,8 @@
 import * as React from "react";
 import Link from "next/link";
 import Image from "next/image";
-
+import SignWhiteNoBCK from "../../public/SignWhiteNoBCK.png"
+import styles from './Navbar.module.css'
 
 const pages = ["About", "Lyrics", "Merch", "Music", "Who are we?"];
 
@@ -11,12 +12,11 @@ export default function Navbar(): React.JSX.Element {
         <nav>
             <div className="flex items-center justify-between p-6 mx-auto text-gray-600 dark:text-gray-300">
                 <div className="flex w-screen gap-3">
-                    <Link href="/">
+                    <Link className={styles.imgContainer} href="/">
                         <Image
-                            src="/images/logo.png"
-                            alt="Logo"
-                            width={50}
-                            height={50}
+                            src={SignWhiteNoBCK}
+                            alt="Unlucky Fam"
+                            className={styles.unluckySignLogo}
                         />
                     </Link>
                     <div className="flex items-center w-screen justify-center gap-10">
