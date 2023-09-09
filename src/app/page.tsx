e5669b2660e7e2647bf408c776e83d310bb6a672
Al366io <alessio.nannipieri@viaplaygroup.com>
2023-09-09 14:56:44 +0200

    wip: threeJSLogo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./default.module.css"
+import ThreeScene from '../../components/ThreeScene'
 
 export default function Home() {
     return (
         <div className={styles.LogoContainer3D}>
-            <video className={styles.video} autoPlay loop playsInline muted src="/Logo3d.mp4"> </video>
+            {/* <video className={styles.video} autoPlay loop playsInline muted src="/Logo3d.mp4"> </video> */}
+            <ThreeScene></ThreeScene>
         </div>
     );
 }
