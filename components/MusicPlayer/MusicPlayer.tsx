4b78ad3ba11057f7f71ffe5932a83a0e03458bb0
Al366io <alessio.nannipieri@viaplaygroup.com>
2023-09-16 18:05:30 +0200

    feat: adds logic for loading iframe

--- components/MusicPlayer/MusicPlayer.tsx
+++ components/MusicPlayer/MusicPlayer.tsx
@@ -4,6 +4,6 @@ import MusicPlayerStyles from "./MusicPlayer.module.css";
 
 export default function MusicPlayer(): React.JSX.Element {
     return <>
-        <iframe className={MusicPlayerStyles.iframe} src="https://open.spotify.com/embed/artist/1toypnVY7Dbdp6VHDJRpxL?utm_source=generator" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        <iframe className={MusicPlayerStyles.iframe} src="https://open.spotify.com/embed/artist/1toypnVY7Dbdp6VHDJRpxL?utm_source=generator" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="eager"></iframe>
     </>;
 }
