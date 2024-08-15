05d9045d874bcfda8bcfa4edaa9b6a9a7a1f9d36
Al366io <alessio.nannipieri@viaplaygroup.com>
2024-08-15 13:15:34 +0200

    fix spotify embed

--- components/MusicPlayer/MusicPlayer.tsx
+++ components/MusicPlayer/MusicPlayer.tsx
@@ -3,13 +3,13 @@ import MusicPlayerStyles from "./MusicPlayer.module.css";
 
 export default function MusicPlayer(): React.JSX.Element {
     return (
-        <>
+        <div className={MusicPlayerStyles.wrapper}>
             <iframe
                 className={MusicPlayerStyles.iframe}
                 src="https://open.spotify.com/embed/artist/1toypnVY7Dbdp6VHDJRpxL?utm_source=generator"
                 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                 loading="eager"
             ></iframe>
-        </>
+        </div>
     );
 }
