39f2f0a0f8bcb06eb99331284ff4ebfc477f30d4
Al366io <alessio.nannipieri@viaplaygroup.com>
2024-08-27 17:04:55 +0200

    add

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,18 @@
+"use client"
 import Image from "next/image";
 
 export default function About() {
+    const openInBrowser = () => {
+        const url = window.location.href;
+        window.open(url, '_blank');
+      };
     return (
         <div className="flex h-screen items-center justify-center">
             Just 3 guys making music for u
+            <hr />
+            <button onClick={openInBrowser} style={{ zIndex: 1000 }}>
+              Open in Browser
+            </button>
         </div>
     );
 }
