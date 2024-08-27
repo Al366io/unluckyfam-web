999400c2d4e8db9361500624e6aa0bc0f48b0f97
Al366io <alessio.nannipieri@viaplaygroup.com>
2024-08-27 17:12:44 +0200

    tryfix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,17 +2,11 @@
 import Image from "next/image";
 
 export default function About() {
-    const openInBrowser = () => {
-        const url = window.location.href;
-        window.open(url, '_blank');
-      };
     return (
         <div className="flex h-screen items-center justify-center">
             Just 3 guys making music for u
             <hr />
-            <button onClick={openInBrowser} style={{ zIndex: 1000 }}>
-              Open in Browser
-            </button>
+            <a href={location.href} target='_blank' download>Open in browser</a>
         </div>
     );
 }
