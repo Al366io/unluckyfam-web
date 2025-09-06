eaf61b066eacedcac16af59243f028561d105a7d
Al366io <alessio.nannipieri0@gmail.com>
2025-09-06 15:07:45 +0200

    fix

---
+++ src/app/secretarea-samply/page.tsx
@@ -0,0 +1,8 @@
+export default function SecretAreaSamply() {
+    return (
+        <div>
+            <h1>Secret Area Samply</h1>
+            <p>This is a placeholder for the Secret Area Samply page.</p>
+        </div>
+    );
+}
